ff148231b0712a1dddd5882109d9e2942c49dcda
Nj147 <nathanjackson147@gmail.com>
2022-11-01 13:16:14 +0000

    DDCNL-6638 LibDependencies formatted

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -7,7 +7,7 @@ object LibDependencies {
     "com.typesafe.play"      %% "play"               % PlayVersion.current,
     "com.vladsch.flexmark"    % "flexmark-all"       % "0.64.0"            % Test,
     "org.jsoup"               % "jsoup"              % "1.15.3"            % Test,
-    "org.scalatest"          %% "scalatest"          % "3.2.14"             % Test,
+    "org.scalatest"          %% "scalatest"          % "3.2.14"            % Test,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"             % Test
   )
