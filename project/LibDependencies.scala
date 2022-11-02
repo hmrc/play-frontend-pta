0c562b6dc3f9cb4049aafb69386e59470992ab12
Nj147 <47452401+Nj147@users.noreply.github.com>
2022-11-02 16:39:36 +0000

    DDCNL-6638 Scala, sbt and plugin versions bumped (#6)
    * DDCNL-6638 Scala, sbt and plugin verisons bumped
    
    * DDCNL-6638 More version bumps and deprecations removed from build.sbt
    
    * DDCNL-6638 Formatting on compile added
    
    * DDCNL-6638 Formatting build.sbt
    
    * DDCNL-6638 LibDependencies updated, test classes formatted
    
    * DDCNL-6638 LibDependencies formatted
    
    * DDCNL-6638 Removed redundant warning silencers
    
    * DDCNL-6638 Flexmark dependency bumped down slightly
    
    * DDCNL-6638 Version changes
    
    * DDCNL-6638 Redundant scalacOption removed

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -5,9 +5,9 @@ import sbt.ModuleID
 object LibDependencies {
   def apply(): Seq[ModuleID] = Seq(
     "com.typesafe.play"      %% "play"               % PlayVersion.current,
-    "com.vladsch.flexmark"    % "flexmark-all"       % "0.35.10"           % Test,
-    "org.jsoup"               % "jsoup"              % "1.13.1"            % Test,
-    "org.scalatest"          %% "scalatest"          % "3.2.3"             % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"       % "0.62.0"            % Test,
+    "org.jsoup"               % "jsoup"              % "1.15.3"            % Test,
+    "org.scalatest"          %% "scalatest"          % "3.2.14"            % Test,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"             % Test
   )
